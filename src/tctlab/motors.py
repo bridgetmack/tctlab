48578a44614819dca724807eb32be53325d23214
bridgetmack <bridgetmack47@gmail.com>
2025-06-17 16:03:22 -0400

    Update motors.py

--- src/tctlab/motors.py
+++ src/tctlab/motors.py
@@ -8,32 +8,41 @@ from progress.bar import Bar
 
 import libximc.highlevel as ximc
 
-# cur_dir = os.path.abspath(os.path.dirname(__file__)) #makes variable that lists current directory
-# ximc_dir = os.path.join(cur_dir, "..", "..", "..", "ximc") #adds directory that ximc is in
-# ximc_package_dir = os.path.join(ximc_dir, "crossplatform", "wrappers", "python") #gets us into the directory that the python library is in
-# sys.path.append(ximc_dir) #adds library to python path
-
-# #add DLLs to path
-# arch_dir = os.path.join(cur_dir, "ximc", "win64")
-# # libdir = os.path.join(ximc_dir, arch_dir)
-# os.add_dll_directory(arch_dir)
-# os.add_dll_directory(cur_dir)
-# try: 
-#     from pyximc import *
-# except ImportError as err:
-#     print("Cannot import pyximc module.")
-#     exit
-# except OSError as err:
-#     if err.winerror == 193:
-#         print("Bit depth does not correspond with operating system")
-#     elif err.winerror == 126:
-#         print("One of the library files is missing")
-#     else:
-#         print(err)
-
-# sbuf= create_string_buffer(64)
-# lib.ximc_version(sbuf)
+z_limit = 39000
 
 def test_library():
     print("Library version: ", ximc.ximc_version())
 
+probe_flags = ximc.EnumerateFlags.ENUMERATE_ALL_COM
+enum_hits = "addr="
+dev_enum= ximc.enumerate_devices(probe_flags, enum_hits)
+
+def test_enum_handles():
+    '''prints device enumeration handles'''
+    print("Device Enumeration Handle: ", repr(dev_enum))
+    print("Device Enumeration Handle Type: ", repr(type(dev_enum)))
+
+# dev_count = ximc.get_device_count(dev_enum)
+
+# def test_dev_count():
+#     '''prints number of devices library finds'''
+#     print('Device Count: ' + repr(dev_count))
+
+controller_name= ximc.controller_name_t()
+x= ximc.get_device_name(dev_enum, 1) ## COM 3
+y= ximc.get_device_name(dev_enum, 2) ## COM 4
+z= ximc.get_device_name(dev_enum, 3) ## COM 5
+
+def test_controller_names():
+    '''prints the com devices the library finds and enumerates'''
+    print("Enumerated Devices: " + str(x), str(y), str(z))
+
+def encode_controller_name(motor):
+    '''encodes the motor name into a form that open_device can read'''
+    if type(motor) is str:
+        motor = motor.encode()
+    return motor
+
+motor_x= ximc.open_device(encode_controller_name(x))
+motor_y= ximc.open_device(encode_controller_name(y))
+motor_z= ximc.open_device(encode_controller_name(z))
