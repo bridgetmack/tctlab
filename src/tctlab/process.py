2b4feacce9658bdd1fecd0a404856226ba21572a
bridgetmack <bridgetmack47@gmail.com>
2026-05-27 13:42:02 -0400

    fjof

--- src/tctlab/process.py
+++ src/tctlab/process.py
@@ -51,9 +51,9 @@ def matrices(datalocation, date, channel, nn):
         pp.append([int(xx[i]), int(yy[i])])
         ww.append([functs.waveforms.avg_waveform(datalocation, date, channel, int(xx[i]), int(yy[i]), nn)[3], functs.waveforms.avg_waveform(datalocation, date, channel, int(xx[i]), int(yy[i]), nn)[4], functs.waveforms.avg_waveform(datalocation, date, channel, int(xx[i]), int(yy[i]), nn)[5]])
         print(i)
-
-    ww= np.array(ww)
     print(ww)
+    ww= np.array(ww)
+    
     np.save(f"{datalocation}/scan_wfms{channel}.npy", ww)
 
 def single_pt(datalocation, date, channel, nn):
