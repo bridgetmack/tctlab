0d973b6b6be4ad0b8edbe0fbd8285cbf89f385e3
bridgetmack <bridgetmack47@gmail.com>
2026-05-27 15:06:58 -0400

    fda

--- src/tctlab/functs.py
+++ src/tctlab/functs.py
@@ -33,13 +33,10 @@ class waveforms:
     def avg_waveform(datalocation, date, channel, x, y, nn):
         '''For input pointer, returns the average waveform and its features'''
         channel, x, y, wf_t, wf_vs = waveforms.import_waveform(datalocation, date, channel, x, y)
-        print(len(wf_vs))
-        print(len(wf_vs[1]))
 
         wf_v = np.mean(wf_vs, axis=1)
         wf_v = wf_v - np.mean(wf_v[:100])
         wf_v = list(wf_v)
-        #print(wf_v)
        
         wf_stdev = np.std(wf_vs, axis=1) / np.sqrt(nn)
 
