acdfea1f73943fdea33b882fcea3263aa3ebf974
bridgetmack <bridgetmack47@gmail.com>
2026-06-23 15:29:13 -0400

    fdas

--- src/tctlab/functs.py
+++ src/tctlab/functs.py
@@ -105,11 +105,11 @@ class waveforms:
                 wfm_chunk = wfm[rough_i - 10:rough_i + 20]
                 t_chunk = t[rough_i - 10:rough_i + 20]
                 
-                yparams, ycov = curve_fit(fits.gaus_func, t_chunk, wfm_chunk)
+                yparams, ycov = curve_fit(fits.find_land, t_chunk, wfm_chunk)
                 
                 yfit = []
                 for i in range(len(t_chunk)):
-                    yfit.append(fits.gaus_func(t_chunk[i], *yparams))
+                    yfit.append(fits.find_land(t_chunk[i], *yparams))
                 
                 plt.plot(t_chunk, wfm_chunk)
                 plt.plot(t_chunk, yfit, 'm.')
