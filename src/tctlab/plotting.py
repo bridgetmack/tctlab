a5d08907288d928a115b20a5d1816a630445c1e7
bridgetmack <bridgetmack47@gmail.com>
2026-08-24 11:53:33 -0400

    fdsa

--- src/tctlab/plotting.py
+++ src/tctlab/plotting.py
@@ -337,7 +337,7 @@ def weighted_avg_hist(datalocation, plotlocation, date):
         
         h, xbins, xpatches = plt.hist(wax, bins=100, color='purple', edgecolor='black', label=f'mean = {round(np.mean(wax), 3)} \n$\sigma$ = {round(np.std(wax), 3)}')
         xplt = norm.pdf(xbins, xmu, xsigma)
-        plt.plot(xbins, xplt, 'r--', linewidth=2, label=f"mu={xmu}, sigma={xsigma}")
+        plt.plot(xbins, xplt, 'b--')
         plt.legend()
         plt.title(f'Reconstructed X; True x = {int(ux[i])}')
         plt.xlabel('Reconstructed X (weighted average)')
@@ -346,7 +346,7 @@ def weighted_avg_hist(datalocation, plotlocation, date):
         
         hy, ybins, ypatches = plt.hist(way, bins= 100, color='purple', edgecolor='black', label=f'mean = {round(np.mean(way), 3)} \n$\sigma$ = {round(np.std(way), 3)}')
         yplt = norm.pdf(ybins, ymu, ysigma)
-        plt.plot(ybins, yplt, 'b--', linewidth=2, label=f"mu={ymu}, sigma={ysigma}")
+        plt.plot(ybins, yplt, 'b--')
         plt.legend()
         plt.title(f'Reconstructed Y; True y = {int(uy[i])}')
         plt.xlabel('Reconstructed Y (weighted average)')
