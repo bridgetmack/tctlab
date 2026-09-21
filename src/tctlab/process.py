cb79c686f22249cc7b12dd6327edd056e96c0a05
bridgetmack <bridgetmack47@gmail.com>
2026-09-21 13:18:15 -0400

    fdsa

--- src/tctlab/process.py
+++ src/tctlab/process.py
@@ -116,7 +116,10 @@ def convert(datalocation, date, nn, channel_tags):
             
             t = np.linspace(0, nsamples*10, nsamples)
             for j in range(len(cc)):
-                c0[j-2,:] = cc[j]
+                try:
+                    c0[j-1,:] = cc[j]
+                except:
+                    print(j)
                 
             np.savetxt(f"{datalocation}/chan{channel}v{date}-x{int(xx[x])}-y{int(yy[x])}.txt", np.transpose(cc))        
             np.savetxt(f"{datalocation}/chan{channel}t{date}-x{int(xx[x])}-y{int(yy[x])}.txt", t/1000)
